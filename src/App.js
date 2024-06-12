d0240a3cba28b6b12dc0afa9b670358cf9aeecb6
Haritha Himansha <143889589+hhimansha@users.noreply.github.com>
2024-06-12 21:36:20 +0530

    so far so good

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import LandingPage from './components/landingPage';
+import NavBar from './components/navBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <LandingPage/>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,27 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        'primary': '#29B6F6',
+        'grey': 'rgb(40, 40, 40)',
+        'grey-mid': '#343432',
+        'grey-light': '#edeff1',
+      },
+      textColor: {
+        'primary': '#29B6F6',
+      },
+      screens: {
+        'xs': {'max': '412px'}
+      },
+      width: {
+        '242': '242px',
+        'mid' : '500px',
+        'fit': '1280px',
+        'wide': '1800px',
+      },
+    },
+   
   },
   plugins: [],
 }
